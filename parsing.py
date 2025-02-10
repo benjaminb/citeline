91011d6bfb000b50fcc30e24c19ea4ad9dcf42c2
benjaminb <benbasseri@gmail.com>
2025-02-10 16:10:51 -0500

    testing the inline citation regex

--- parsing.py
+++ parsing.py
@@ -1,4 +1,16 @@
 import pysbd
+import re
+
+# Regex for inline citations
+lastname = r"[A-Z][a-zA-ZÀ-ÖØ-öø-ÿ-]*(?:'[A-Z][a-zA-ZÀ-ÖØ-öø-ÿ-]*)?"
+year = r"\(?(\d{4}[a-z]?)\)?"
+name_sep = r",?\s"
+INLINE_CITATION_REGEX = re.compile(
+    fr"({lastname}(?:{name_sep}{lastname})*(?: et al.?)?)\s*{year}")
+
+
+def get_inline_citations(text: str) -> list[tuple[str, str]]:
+    return [match.groups() for match in INLINE_CITATION_REGEX.finditer(text)]
 
 
 def segment_sentences(text: str) -> list[str]:
