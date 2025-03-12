d08656a2a919ffc24120a0f21633f6303833fdfb
benjaminb <benbasseri@gmail.com>
2025-03-12 10:30:41 -0400

    adds slurm dir for slurm batch jobs

---
+++ slurm/create_bge_table.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH -p seas_gpu # partition (queue)
+#SBATCH -c 20 # number of cores
+#SBATCH --gres=gpu:2 # number of GPUs
+#SBATCH --mem 64000 # memory pool for all cores
+#SBATCH -t 0-3:30 # time (D-HH:MM)
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
+module load python
+mamba activate citeline
+cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline/database
+git pull
+python database.py --create-vector-table --embedder="BAAI/bge-small-en-foo" --batch-size=256
