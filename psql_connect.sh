58cdcf374f7c074a85709b82fff2a507086e0f3c
benjaminb <benbasseri@gmail.com>
2025-03-16 11:50:31 -0400

    psql_connect.sh sources .env

--- psql_connect.sh
+++ psql_connect.sh
@@ -1,1 +1,2 @@
+source .env
 singularity exec --env PGHOST=${DB_HOST}.rc.fas.harvard.edu,PGPORT=${DB_PORT},PGDATABASE=${DB_NAME},PGUSER=${DB_USER},PGPASSWORD=${DB_PASSWORD} /n/netscratch/informatics/Everyone/nweeks/ticket/INC05791924/pgvector_0.8.0-pg17.sif psql
