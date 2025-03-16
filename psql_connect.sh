10c3aa550e2d62aab20ea41bfdfdf71c5ccf00de
benjaminb <benbasseri@gmail.com>
2025-03-16 11:51:11 -0400

    bug fixes

--- psql_connect.sh
+++ psql_connect.sh
@@ -1,2 +1,2 @@
 source .env
-singularity exec --env PGHOST=${DB_HOST}.rc.fas.harvard.edu,PGPORT=${DB_PORT},PGDATABASE=${DB_NAME},PGUSER=${DB_USER},PGPASSWORD=${DB_PASSWORD} /n/netscratch/informatics/Everyone/nweeks/ticket/INC05791924/pgvector_0.8.0-pg17.sif psql
+singularity exec --env PGHOST=${DB_HOST},PGPORT=${DB_PORT},PGDATABASE=${DB_NAME},PGUSER=${DB_USER},PGPASSWORD=${DB_PASSWORD} /n/netscratch/informatics/Everyone/nweeks/ticket/INC05791924/pgvector_0.8.0-pg17.sif psql
