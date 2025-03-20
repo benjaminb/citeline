355eedd9a74e57ed4326bd2d72240c9dc3535331
benjaminb <benbasseri@gmail.com>
2025-03-19 23:27:56 -0400

    revises slurms

--- slurm/create_astrobert_column.sh
+++ slurm/create_astrobert_column.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-#SBATCH -p sapphire # partition (queue)
-#SBATCH -c 20 # number of cores
-#SBATCH --gres=gpu:1 # number of GPUs
+#SBATCH -p gpu # partition (queue)
+#SBATCH -c 12 # number of cores
+#SBATCH --gres=gpu:2 # number of GPUs
 #SBATCH --mem 64000 # memory pool for all cores
-#SBATCH -t 0-3:30 # time (D-HH:MM)
+#SBATCH -t 0-6:00 # time (D-HH:MM)
 #SBATCH -o slurm.%N.%j.out # STDOUT
 #SBATCH -e slurm.%N.%j.err # STDERR
 module load python
