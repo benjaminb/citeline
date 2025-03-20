ef90bde126391be9207d4d4d69f44a51456d3e06
benjaminb <benbasseri@gmail.com>
2025-03-20 10:52:09 -0400

    slurms updates

--- slurm/create_bge_column.sh
+++ slurm/create_bge_column.sh
@@ -6,15 +6,14 @@
 #SBATCH --gres=gpu:2 # number of GPUs
 #SBATCH --mem 64000 # memory pool for all cores
 #SBATCH -t 0-6:00 # time (D-HH:MM)
-#SBATCH -o slurm.%x.%N.%j.out # STDOUT
-#SBATCH -e slurm.%x.%N.%j.err # STDERR
+#SBATCH -o slurm.%x.%N.%j.log # STDOUT
+#SBATCH -e slurm.%x.%N.%j.log # STDERR
 module load python
 mamba deactivate
 mamba activate citeline
 echo "which python: $(which python)"
 cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline/database
 git pull
-echo "starting python job..."
-python database.py create-vector-table --table-name="library" --target-column-name chunk --embedder="BAAI/bge-small-en" --normalize --batch-size=256
+python database.py --create-vector-column --table-name="library" --target-column-name chunk --embedder="BAAI/bge-small-en" --normalize --batch-size=256
 timestamp=$(date +"%Y%m%d_%H%M%S")
 echo "ended at: $timestamp"
