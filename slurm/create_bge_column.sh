5a171172255e40a6b7df145e5e78fd155d6e4295
benjaminb <benbasseri@gmail.com>
2025-03-20 10:46:16 -0400

    updating slurm

--- slurm/create_bge_column.sh
+++ slurm/create_bge_column.sh
@@ -6,16 +6,15 @@
 #SBATCH --gres=gpu:2 # number of GPUs
 #SBATCH --mem 64000 # memory pool for all cores
 #SBATCH -t 0-6:00 # time (D-HH:MM)
-#SBATCH -o slurm.%N.%j.%x.out # STDOUT
-#SBATCH -e slurm.%N.%j.%x.err # STDERR
+#SBATCH -o slurm.%x.%N.%j.out # STDOUT
+#SBATCH -e slurm.%x.%N.%j.err # STDERR
 module load python
 mamba deactivate
 mamba activate citeline
 echo "which python: $(which python)"
 cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline/database
 git pull
-timestamp=$(date +"%Y%m%d_%H%M%S")
-echo "create_bge_column.sh: $timestamp"
+echo "starting python job..."
 python database.py create-vector-table --table-name="library" --target-column-name chunk --embedder="BAAI/bge-small-en" --normalize --batch-size=256
 timestamp=$(date +"%Y%m%d_%H%M%S")
 echo "ended at: $timestamp"
--- slurm/joblist.sh
+++ slurm/joblist.sh
@@ -1,2 +1,2 @@
 sacct -u bbasseri --format=JobId,Partition,NCPUS,ReqTRES,ReqMem,Start,End,State | \
-awk 'NR==1{sub("ReqTRES", "GPU")} {gsub(/billing=[0-9]+\+/, substr($4, 9, length($4)-9))}1'
+awk 'NR==1{sub("ReqTRES", "GPUs")} {gsub(/billing=/, "    "); gsub(/\+/, "    ");}1'
