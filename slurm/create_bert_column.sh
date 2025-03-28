c69db2e22a85b80b1e14490b87a7c1bbacd12a4e
benjaminb <benbasseri@gmail.com>
2025-03-28 16:36:38 -0400

    updates slurms

--- slurm/create_bert_column.sh
+++ slurm/create_bert_column.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
-#SBATCH --job-name=create_bert_column
+#SBATCH --job-name=bert_vector_column
 #SBATCH -p gpu # partition (queue)
-#SBATCH -c 12 # number of cores
+#SBATCH -c 4 # number of cores
 #SBATCH --gres=gpu:2 # number of GPUs
 #SBATCH --mem 64000 # memory pool for all cores
-#SBATCH -t 0-6:00 # time (D-HH:MM)
+#SBATCH -t 0-03:00 # time (D-HH:MM)
 #SBATCH -o slurm.%x.%t.log # STDOUT
 #SBATCH -e slurm.%x.%t.log # STDERR
 module load python
 mamba deactivate && mamba activate citeline
 cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline/database
 git pull
-python database.py --create-vector-column --table-name="library" --target-column-name chunk --embedder="bert-base-uncased" --batch-size=256
+python database.py --create-vector-column --table-name="lib" --target-column chunk --embedder="bert-base-uncased" --batch-size=64
 timestamp=$(date +"%Y%m%d_%H%M%S")
 echo "ended at: $timestamp"
