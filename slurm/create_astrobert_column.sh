25b7d75bd3cb060d2c7ced22475d429f6f2f3cf2
benjaminb <benbasseri@gmail.com>
2025-03-28 21:26:45 -0400

    slurms

--- slurm/create_astrobert_column.sh
+++ slurm/create_astrobert_column.sh
@@ -13,6 +13,7 @@ mamba deactivate && mamba activate citeline
 cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline
 git pull
 ./pg_start.sh
+cd database
 python database.py --create-vector-column --table-name="lib" --target-column-name chunk --embedder="adsabs/astroBERT" --batch-size=128
 timestamp=$(date +"%Y%m%d_%H%M%S")
 echo "ended at: $timestamp"
