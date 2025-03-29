71e0d80c1016900e06c62db9a67e00131a83d565
benjaminb <benbasseri@gmail.com>
2025-03-29 00:12:37 -0400

    slurms

--- slurm/create_bge_column.sh
+++ slurm/create_bge_column.sh
@@ -2,16 +2,18 @@
 #
 #SBATCH --job-name=bge_vector_column
 #SBATCH -p gpu # partition (queue)
-#SBATCH -c 4 # number of cores
+#SBATCH -c 12 # number of cores
 #SBATCH --gres=gpu:2 # number of GPUs
 #SBATCH --mem=64GB # memory pool for all cores
-#SBATCH -t 0-03:00 # time (D-HH:MM)
+#SBATCH -t 0-04:00 # time (D-HH:MM)
 #SBATCH -o slurm.%x.%t.log # STDOUT
 #SBATCH -e slurm.%x.%t.log # STDERR
 module load python
 mamba deactivate && mamba activate citeline
-cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline/database
+cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline
 git pull
-python database.py --create-vector-column --table-name="lib" --target-column chunk --embedder="BAAI/bge-small-en" --batch-size=64
+./pg_start.sh
+cd database
+python database.py --create-vector-column --table-name="lib" --target-column chunk --embedder="BAAI/bge-small-en" --batch-size=128
 timestamp=$(date +"%Y%m%d_%H%M%S")
 echo "ended at: $timestamp"
