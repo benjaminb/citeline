8330ff3bcd2bef6f28e740d02c5da0517e9acc2c
benjaminb <benbasseri@gmail.com>
2025-04-22 11:25:33 -0400

    add test script

--- database/test_query.py
+++ database/test_query.py
@@ -1,9 +1,10 @@
 from database import Database
 from time import time
 
-
+start = time()
 db = Database()
 db.test_connection()
+print(f"Connection took {time() - start:.3f} seconds")
 
 start = time()
 results = db.query("SELECT id, doi, chunk FROM lib;")
