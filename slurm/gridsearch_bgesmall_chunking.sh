aa4376e201afe292b188eb1834a4dfde7cd0b4c9
benjaminb <benbasseri@gmail.com>
2025-05-04 19:33:42 -0400

    add chunking slurms

---
+++ slurm/gridsearch_bgesmall_chunking.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=gridsearch_bert_chunking
+#SBATCH -p gpu # partition (queue)
+#SBATCH -c 2 # number of cores
+#SBATCH --gres=gpu:1 # number of GPUs
+#SBATCH --mem=16GB # memory pool for all cores
+#SBATCH -t 0-02:00 # time (D-HH:MM)
+#SBATCH -o slurm.%x.%t.log # STDOUT
+#SBATCH -e slurm.%x.%t.log # STDERR
+module load python
+mamba deactivate && mamba activate citeline
+echo "which python: $(which python)"
+cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline
+git pull
+cd chunking
+python chunking_grid_search.py --model_name="nasa-impact/nasa-ibm-st.38m"
+timestamp=$(date +"%Y%m%d_%H%M%S")
+echo "ended at: $timestamp"
