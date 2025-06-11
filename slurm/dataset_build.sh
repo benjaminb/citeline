d372316e07b4e2abdf60cab0a3d3d19e82d9a843
benjaminb <benbasseri@gmail.com>
2025-06-11 19:58:59 -0400

    typos

--- slurm/dataset_build.sh
+++ slurm/dataset_build.sh
@@ -18,7 +18,7 @@ git pull
 # export OLLAMA_BASE_URL=http://localhost:11434
 export TMPDIR=/tmp
 podman load -i /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/ollama_llama3.3.tar
-podman run -d --name ollama-server --log-level=debug --rm  --userns=keep-id --device nvidia.com/gpu=all -p 11434:11434 ollamaserve
+podman run -d --name ollama-server --log-level=debug --rm --userns=keep-id --device nvidia.com/gpu=all -p 11434:11434 ollamaserve
 
 
 # Trigger the model to load
