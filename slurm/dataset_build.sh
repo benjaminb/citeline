09aef21ddb71904a8f2f3b9857a20b61e0869563
benjaminb <benbasseri@gmail.com>
2025-06-11 14:36:38 -0400

    update slurms

--- slurm/dataset_build.sh
+++ slurm/dataset_build.sh
@@ -8,8 +8,8 @@
 #SBATCH -t 0-00:30 # time (D-HH:MM)
 #SBATCH -o slurm.%x.%j.log # STDOUT
 #SBATCH -e slurm.%x.%j.log # STDERR
-module load python
-mamba deactivate && mamba activate citeline
+# module load python
+# mamba deactivate && mamba activate citeline
 echo "which python: $(which python)"
 cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline
 git pull
