25ebd2c78c106baca6d7a5e23b7c363f00eebeb5
benjaminb <benbasseri@gmail.com>
2025-06-13 10:14:09 -0400

    slurms

--- slurm/dataset_build.sh
+++ slurm/dataset_build.sh
@@ -8,8 +8,7 @@
 #SBATCH -t 0-00:45 # time (D-HH:MM)
 #SBATCH -o slurm.%x.%j.log # STDOUT
 #SBATCH -e slurm.%x.%j.log # STDERR
-# module load python
-# mamba deactivate && mamba activate citeline
+
 echo "which python: $(which python)"
 cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline
 git pull
@@ -21,23 +20,25 @@ podman load -i /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/ollama_llama3.3.tar
 podman run -d --name ollama-server --log-level=debug --rm --userns=keep-id --device nvidia.com/gpu=all -p 11434:11434 ollamaserve
 
 sleep 60
+echo "After 1 minute..."
+podman container list
 # Trigger the model to load
 curl http://localhost:11434/api/generate -d '{
   "model": "llama3.3:latest",
   "prompt": "Respond with a single word that is the name of a fruit."
 }'
 
-echo "Waiting for Ollama model to load..."
-while true; do
-    # Check if we can exec into the container and if ollama ps shows a loaded model
-    if podman exec ollama-server ollama ps 2>/dev/null | grep -q "llama3.3:latest"; then
-        echo "Model loaded successfully!"
-        break
-    else
-        echo "Model not loaded yet, waiting 30 seconds..."
-        sleep 30
-    fi
-done
+# echo "Waiting for Ollama model to load..."
+# while true; do
+#     # Check if we can exec into the container and if ollama ps shows a loaded model
+#     if podman exec ollama-server ollama ps 2>/dev/null | grep -q "llama3.3:latest"; then
+#         echo "Model loaded successfully!"
+#         break
+#     else
+#         echo "Model not loaded yet, waiting 30 seconds..."
+#         sleep 30
+#     fi
+# done
 
 cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline
 python dataset_builder.py
