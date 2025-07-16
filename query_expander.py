5e48180d1fb9f4f2185d0e0b3e31a05e284cba7d
benjaminb <benbasseri@gmail.com>
2025-07-15 20:16:10 -0400

    TextEnricher -> query_expander

--- query_expander.py
+++ query_expander.py
@@ -20,11 +20,11 @@ import pandas as pd
 
 def query_expander_factory(keys_and_headers: list[tuple[str, str]], prev_n: int = 0):
     """
-    Factory function to create an enrichment function based on the keys and number of previous sentences
+    Factory function to create an expansion function based on the keys and number of previous sentences
     """
 
     # NOTE: changing this to take an example (pd.Series) rather than just the text
-    def enrichment_function(example, record: pd.Series | dict) -> str:
+    def query_expansion_function(example, record: pd.Series | dict) -> str:
 
         text = example.sent_no_cit
         if prev_n > 0:
@@ -38,42 +38,35 @@ def query_expander_factory(keys_and_headers: list[tuple[str, str]], prev_n: int 
 
         # Add the header and corresponding value
         if keys_and_headers:
-            header_text = "\n\n".join(
-                [f"{header}\n{record[key]}" for key, header in keys_and_headers]
-            )
+            header_text = "\n\n".join([f"{header}\n{record[key]}" for key, header in keys_and_headers])
             text = f"IMPORTANT:\n{text}\n\n{header_text}" if header_text else text
 
         return text
 
-    return enrichment_function
+    return query_expansion_function
 
 
-class TextEnricher:
-    ENRICHMENT_FN = {
+class QueryExpander:
+    EXPANSION_FN = {
         "identity": query_expander_factory([]),
         "add_abstract": query_expander_factory([("abstract", "Abstract:")]),
         "add_title": query_expander_factory([("title", "Title:")]),
-        "add_title_and_abstract": query_expander_factory(
-            [("title", "Title:"), ("abstract", "Abstract:")]
-        ),
+        "add_title_and_abstract": query_expander_factory([("title", "Title:"), ("abstract", "Abstract:")]),
         "add_prev_3": query_expander_factory([], prev_n=3),
         "add_abstract_prev_3": query_expander_factory([("abstract", "Abstract:")], prev_n=3),
         "add_title_prev_3": query_expander_factory([("title", "Title:")], prev_n=3),
-        "add_title_abstract_prev3": query_expander_factory(
-            [("title", "Title:"), ("abstract", "Abstract:")], prev_n=3
-        ),
+        "add_title_abstract_prev3": query_expander_factory([("title", "Title:"), ("abstract", "Abstract:")], prev_n=3),
         "add_prev_7": query_expander_factory([], prev_n=7),
     }
 
     def __init__(
         self,
-        enrichment_function: str,
+        expansion_function_name: str,
         reference_data: None = pd.DataFrame,
-        # query_data: None = pd.DataFrame,
     ):
         """
-        enrichment_function: function
-            Takes an example and a record, and returns the enriched text.
+        expansion_function: function
+            Takes an example and a record, and returns the expanded text.
         query_data: pd.DataFrame
             DataFrame containing the records to query against. These are the Reviews datasets
             and are used to enrich examples during evaluation or query time.
@@ -81,19 +74,19 @@ class TextEnricher:
             DataFrame containing the reference records. These are all the non-Reviews datasets
             and are used to enrich 'chunks' when creating enriched tables on the database
         """
-        if not enrichment_function in self.ENRICHMENT_FN:
+        if not expansion_function_name in self.EXPANSION_FN:
             raise KeyError(
-                f"Enrichment function {enrichment_function} not supported. Available functions: {list(self.ENRICHMENT_FN.keys())}"
+                f"Enrichment function {expansion_function_name} not supported. Available functions: {list(self.EXPANSION_FN.keys())}"
             )
 
-        self.name = enrichment_function
-        self.enricher = self.ENRICHMENT_FN[enrichment_function]
+        self.name = expansion_function_name
+        self.enricher = self.EXPANSION_FN[expansion_function_name]
         self.doi_to_record = reference_data.set_index("doi").to_dict(orient="index")
 
     def __str__(self):
-        return f"TextEnricher(name={self.name}, data_length={len(self.doi_to_record)})"
+        return f"QueryExpander(name={self.name}, data_length={len(self.doi_to_record)})"
 
-    def enrich_batch(self, texts_with_dois: list[tuple[str, str]]) -> list[str]:
+    def expand_batch(self, texts_with_dois: list[tuple[str, str]]) -> list[str]:
         """
         Process multiple texts with their associated DOIs in one batch
 
@@ -105,9 +98,7 @@ class TextEnricher:
         for text, doi in texts_with_dois:
             record = self.doi_to_record.get(doi)
             if record is None:
-                raise ValueError(
-                    f"While enriching example with source doi '{doi}', full record not found"
-                )
+                raise ValueError(f"While enriching example with source doi '{doi}', full record not found")
             results.append(self.enricher(text, record))
 
         return results
@@ -126,26 +117,25 @@ class TextEnricher:
         return results
 
 
-def get_enricher(name: str, path_to_data: str) -> TextEnricher:
+def get_expander(name: str, path_to_data: str) -> QueryExpander:
     try:
         data = pd.read_json(path_to_data, lines=True)
-        return TextEnricher(enrichment_function=name, reference_data=data)
+        return QueryExpander(expansion_function_name=name, reference_data=data)
     except Exception as e:
         print(f"Error loading data source: {e}")
 
 
 def main():
 
-    enricher = get_enricher("identity", for_query=False)
+    expander = get_expander("identity", path_to_data="data/preprocessed/reviews.jsonl")
+    print(f"Expander created: {expander}")
 
-    examples = pd.read_json("data/dataset/small/nontrivial.jsonl", lines=True)
-    batch = [(example.sent_no_cit, example.source_doi) for _, example in examples.iterrows()]
-    batch = batch[:2]
-
-    enriched_batch = enricher.enrich_batch(batch)
-    for sample in enriched_batch[:]:
-        print(sample)
+    for key, value in expander.doi_to_record.items():
+        print(f"DOI: {key}, value: {type(value)}")
+        print(f"Keys in the value dict: {list(value.keys())}")
         print("===" * 20)
+        break
+
 
 
 if __name__ == "__main__":
