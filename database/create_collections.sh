d17bc104023371beb5769c06dfa359e29125e0c9
benjaminb <benbasseri@gmail.com>
2025-08-16 11:32:51 -0400

    unnormalizes create_collections.sh

--- database/create_collections.sh
+++ database/create_collections.sh
@@ -2,10 +2,11 @@ python milvusdb.py --create-collection \
 --name astrollama_chunks \
 --data-source ../data/research_chunks.jsonl \
 --embedder UniverseTBD/astrollama \
---normalize
+# --normalize
 
 python milvusdb.py --create-collection \
 --name astrollama_contributions \
 --data-source ../data/research_contributions.jsonl \
 --embedder UniverseTBD/astrollama \
---normalize
+# --normalize
+
