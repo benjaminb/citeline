8101359a2726b4f93126468122c998b989aa3b73
benjaminb <benbasseri@gmail.com>
2025-08-19 10:31:32 -0400

    adds batch sizes to create_collections.sh

--- database/create_collections.sh
+++ database/create_collections.sh
@@ -9,48 +9,57 @@ python milvusdb.py --create-collection \
 --name bge_chunks \
 --data-source ../data/research_chunks.jsonl \
 --embedder BAAI/bge-large-en-v1.5 \
---normalize
+--normalize \
+--batch-size 32
 
 python milvusdb.py --create-collection \
 --name astrobert_chunks \
 --data-source ../data/research_chunks.jsonl \
 --embedder adsabs/astroBERT \
+--batch-size 32
 # --normalize
 
 python milvusdb.py --create-collection \
 --name astrobert_contributions \
 --data-source ../data/research_contributions.jsonl \
 --embedder adsabs/astroBERT \
+--batch-size 64
 # --normalize
 
 python milvusdb.py --create-collection \
 --name specter_contributions \
 --data-source ../data/research_contributions.jsonl \
 --embedder allenai/specter2 \
+--batch-size 64
 # --normalize
 
 python milvusdb.py --create-collection \
 --name specter_chunks \
 --data-source ../data/research_chunks.jsonl \
 --embedder allenai/specter2 \
+--batch-size 32
 # --normalize
 
 python milvusdb.py --create-collection \
 --name qwen8b_contributions \
 --data-source ../data/research_contributions.jsonl \
 --embedder Qwen/Qwen3-Embedding-8B \
+--batch-size 32
 
 python milvusdb.py --create-collection \
 --name qwen8b_chunks \
 --data-source ../data/research_chunks.jsonl \
 --embedder Qwen/Qwen3-Embedding-8B \
+--batch-size 16
 
 python milvusdb.py --create-collection \
 --name astrollama_contributions \
 --data-source ../data/research_contributions.jsonl \
 --embedder UniverseTBD/astrollama \
+--batch-size 32
 
 python milvusdb.py --create-collection \
 --name astrollama_chunks \
 --data-source ../data/research_chunks.jsonl \
 --embedder UniverseTBD/astrollama \
+--batch-size 16
