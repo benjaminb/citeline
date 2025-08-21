0f0cf567f1764d68613e1f753be41dc9d9d45695
benjaminb <benbasseri@gmail.com>
2025-08-20 22:16:14 -0400

    adds qwen baseline experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,2 +1,2 @@
-python milvus_experiment.py --run experiments/baselines/nasa_contributions.yaml
-python milvus_experiment.py --run experiments/baselines/nasa_chunks.yaml
+python milvus_experiment.py --run experiments/baselines/qwen06_contributions.yaml
+python milvus_experiment.py --run experiments/baselines/qwen06_chunks.yaml
