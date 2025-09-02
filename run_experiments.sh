bdbe826b606bb976e9c34f18ec72b718bf765b25
benjaminb <benbasseri@gmail.com>
2025-09-02 11:27:33 -0400

    updates experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,2 +1,5 @@
 python milvus_experiment.py --run experiments/baselines/astrollama_chunks.yaml
 python milvus_experiment.py --run experiments/baselines/astrollama_contributions.yaml
+python milvus_experiment.py --run experiments/baselines/astrosage_chunks.yaml
+python milvus_experiment.py --run experiments/baselines/astrosage_contributions.yaml
+
