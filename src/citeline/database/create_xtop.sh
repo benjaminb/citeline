cd016038930e4caba032ed5bc3a93bc55d21d431
benjaminb <benbasseri@gmail.com>
2025-10-18 09:08:02 -0400

    sets up more xtop experiments

--- src/citeline/database/create_xtop.sh
+++ src/citeline/database/create_xtop.sh
@@ -12,3 +12,14 @@ python milvusdb.py --create-xtop-collection \
 --embedder Qwen/Qwen3-Embedding-0.6B \
 --normalize \
 --batch-size 32
+
+
+python milvusdb.py --create-xtop-collection \
+--name xtop_1 \
+--from-collection qwen06_chunks \
+--pca-pickle ../../../xtop_pca_1000.pkl \
+--mean-vector-pickle ../../../xtop_mean_vector_1000.pkl \
+--remove-top-n 1 \
+--embedder Qwen/Qwen3-Embedding-0.6B \
+--normalize \
+--batch-size 32
