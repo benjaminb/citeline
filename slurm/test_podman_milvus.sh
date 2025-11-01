8fdbc48e3a6f69d426f2bf556e99ff26b362b858
benjaminb <benbasseri@gmail.com>
2025-11-01 09:41:51 -0400

    test slurm job

---
+++ slurm/test_podman_milvus.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+#
+#SBATCH --job-name=test_running_milvus_with_podman
+#SBATCH -p test # partition (queue)
+#SBATCH -c 2 # number of cores
+#SBATCH --mem=16GB # memory pool for all cores
+#SBATCH -t 0-01:00 # time (D-HH:MM)
+#SBATCH -o slurm.%x.%t.log # STDOUT
+#SBATCH -e slurm.%x.%t.log # STDERR
+source ~/.zshrc
+# module load python
+# mamba deactivate && mamba activate citeline
+echo "which python: $(which python)"
+# cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline
+git pull
+cd chunking
+timestamp=$(date +"%Y%m%d_%H%M%S")
+echo "ended at: $timestamp"
