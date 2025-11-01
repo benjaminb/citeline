45d37d0094052fdb95e02dd8c6933164c25b22be
benjaminb <benbasseri@gmail.com>
2025-11-01 09:57:32 -0400

    testing podman via slurm

--- slurm/test_podman_milvus.sh
+++ slurm/test_podman_milvus.sh
@@ -7,11 +7,19 @@
 #SBATCH -t 0-01:00 # time (D-HH:MM)
 #SBATCH -o slurm.%x.%t.log # STDOUT
 #SBATCH -e slurm.%x.%t.log # STDERR
-source ~/.zshrc
+# source ~/.zshrc
 # module load python
 # mamba deactivate && mamba activate citeline
 echo "which python: $(which python)"
 echo "pwd: $(pwd)"
 git pull
+
+# You start out it the slurm directory
+cd ../src/citeline/database/milvus
+podman compose up -d
+sleep 30
+
+cd ..
+python milvusdb.py --list-collections
 timestamp=$(date +"%Y%m%d_%H%M%S")
 echo "ended at: $timestamp"
