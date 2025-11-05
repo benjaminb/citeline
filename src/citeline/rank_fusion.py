d924ca3c6f4adf565272f2e8877b012e54acec07
benjaminb <benbasseri@gmail.com>
2025-11-05 11:31:20 -0500

    work on rank fusion

--- src/citeline/rank_fusion.py
+++ src/citeline/rank_fusion.py
@@ -9,9 +9,10 @@ from multiprocessing import Pool, cpu_count
 from tqdm import tqdm
 from citeline.statistics import compute_averages, compute_individual_stats
 
-load_dotenv('.env')
-assert os.getenv('CPUS'), "Please set CPUS in your .env file"
-NUM_WORKERS = int(os.getenv('CPUS')) - 1
+load_dotenv(".env")
+assert os.getenv("CPUS"), "Please set CPUS in your .env file"
+NUM_WORKERS = int(os.getenv("CPUS")) - 1
+
 
 def argument_parser():
     """
@@ -20,6 +21,7 @@ def argument_parser():
     parser = argparse.ArgumentParser(description="Rank fusion configuration file")
     parser.add_argument("config", help="Path to the YAML config file")
     return parser.parse_args()
+
 
 class RankFuser:
     """
@@ -79,7 +81,7 @@ class RankFuser:
 
     def _read_row_generator(self):
         """Generator that yields rows from the input file."""
-        with open(self.infile, 'r') as f:
+        with open(self.infile, "r") as f:
             for line in f:
                 yield json.loads(line)
 
@@ -109,9 +111,9 @@ class RankFuser:
                 )
             )
         stat_matrices = {
-            "hitrates": np.vstack([item['hitrate'] for item in reranked_stats]),
-            "ious": np.vstack([item['iou'] for item in reranked_stats]),
-            "recalls": np.vstack([item['recall'] for item in reranked_stats])
+            "average_hitrate_at_k": np.vstack([item["hitrate"] for item in reranked_stats]),
+            "average_iou_at_k": np.vstack([item["iou"] for item in reranked_stats]),
+            "average_recall_at_k": np.vstack([item["recall"] for item in reranked_stats]),
         }
         averages_np = compute_averages(stat_matrices)
         return self._serialize_averages(averages_np)
@@ -131,21 +133,18 @@ def main():
     outdir = config.get("outdir", "experiments/rerankers/results/")
 
     rank_fuser = RankFuser(
-        infile=infile_path,
-        config=reranker_config,
-        top_k=top_k,
-        rrf_k=rrf_k,
-        num_workers=NUM_WORKERS
+        infile=infile_path, config=reranker_config, top_k=top_k, rrf_k=rrf_k, num_workers=NUM_WORKERS
     )
 
     average_stats = rank_fuser.execute()
-    log = {'config': config} | average_stats
+    log = {"config": config} | average_stats
     # Make a dir concatenating the name and the timestamp
     os.makedirs(outdir, exist_ok=True)
     with open(os.path.join(outdir, f"{fusion_name}_rank_fusion_log.json"), "w") as f:
         json.dump(log, f, indent=2)
 
     print(f"Results saved to {outdir}/{fusion_name}_rank_fusion_log.json")
+
 
 if __name__ == "__main__":
     main()
