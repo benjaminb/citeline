42ea1e364662cdfd111ac7328f183dd84a4cfb4d
benjaminb <benbasseri@gmail.com>
2026-02-21 18:37:11 -0500

    updates test_podman slurm

--- slurm/test_podman.sh
+++ slurm/test_podman.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
 #SBATCH --job-name=test_podman
-#SBATCH -p test # partition (queue)
+#SBATCH -p seas_gpu # partition (queue)
 #SBATCH -c 2 # number of cores
+#SBATCH --gres=gpu:1 # number of GPUs
 #SBATCH --mem 64000 # memory pool for all cores
 #SBATCH --time=0-00:30 # time (D-HH:MM)
 #SBATCH -o slurm.%x.%j.log # STDOUT
@@ -23,10 +24,11 @@ export TMPDIR=/n/holylabs/LABS/protopapas_lab/Lab/bbasseri/tmp
 podman load -i /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/ollama_llama3.3.tar
 echo "Images available:"
 podman images
-# podman run -d --log-level=debug --rm --device nvidia.com/gpu=all -p 11434:11434 ollamaserve:latest
-# sleep 30
-# echo "After 30 seconds..."
-# podman container list
+sleep 30
+echo "After 30 seconds..."
+podman run -d --log-level=debug --rm --device nvidia.com/gpu=all -p 11434:11434 ollamaserve:latest
+echo "Containers available:"
+podman container list
 # curl http://localhost:11434/api/generate -d '{"model": "llama3.3:latest", "prompt": "Respond with a single word that is the name of a fruit."}'
 # python dataset_builder.py
 # timestamp=$(date +"%Y%m%d_%H%M%S")
