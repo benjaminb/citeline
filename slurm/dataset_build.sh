f2ba6f39ce573c3158994316a14417453efbe9e8
benjaminb <benbasseri@gmail.com>
2026-02-21 18:20:04 -0500

    adds slurms

--- slurm/dataset_build.sh
+++ slurm/dataset_build.sh
@@ -6,13 +6,15 @@
 #SBATCH --gres=gpu:1 # number of GPUs
 #SBATCH --constraint="a100|h200"
 #SBATCH --mem 64000 # memory pool for all cores
-#SBATCH --time=0-03:00 # time (D-HH:MM)
+#SBATCH --time=0-00:30 # time (D-HH:MM)
 #SBATCH -o slurm.%x.%j.log # STDOUT
 #SBATCH -e slurm.%x.%j.log # STDERR
 
 module load python
 mamba activate citeline
 echo "which python: $(which python)"
+echo "which podman: $(which podman)"
+echo "PATH: $PATH"
 cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline
 git pull
 
@@ -21,17 +23,13 @@ git pull
 export OLLAMA_BASE_URL=http://localhost:11434
 export TMPDIR=/n/holylabs/LABS/protopapas_lab/Lab/bbasseri/tmp
 podman load -i /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/ollama_llama3.3.tar
+echo "Images available:"
+podman images
 podman run -d --log-level=debug --rm --device nvidia.com/gpu=all -p 11434:11434 ollamaserve:latest
-
-# Trigger model to load into memory
-sleep 60
-echo "After 1 minute..."
+sleep 30
+echo "After 30 seconds..."
 podman container list
-curl http://localhost:11434/api/generate -d '{
-  "model": "llama3.3:latest",
-  "prompt": "Respond with a single word that is the name of a fruit."
-}'
-
+curl http://localhost:11434/api/generate -d '{"model": "llama3.3:latest", "prompt": "Respond with a single word that is the name of a fruit."}'
 python dataset_builder.py
 timestamp=$(date +"%Y%m%d_%H%M%S")
 echo "ended at: $timestamp"
