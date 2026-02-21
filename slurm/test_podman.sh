9fdbcaff278c9681e059964174941f7f69dd733e
benjaminb <benbasseri@gmail.com>
2026-02-21 18:20:04 -0500

    adds slurms

--- slurm/test_podman.sh
+++ slurm/test_podman.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 #
 #SBATCH --job-name=test_podman
-#SBATCH -p sapphire # partition (queue)
+#SBATCH -p test # partition (queue)
 #SBATCH -c 2 # number of cores
-#SBATCH --gres=gpu:0 # number of GPUs
-#SBATCH --mem 16000 # memory pool for all cores
-#SBATCH -t 0-00:30 # time (D-HH:MM)
+#SBATCH --mem 64000 # memory pool for all cores
+#SBATCH --time=0-00:30 # time (D-HH:MM)
 #SBATCH -o slurm.%x.%j.log # STDOUT
 #SBATCH -e slurm.%x.%j.log # STDERR
 
 module load python
-mamba activate
 mamba activate citeline
 echo "which python: $(which python)"
-cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline/
+echo "which podman: $(which podman)"
+echo "PATH: $PATH"
+cd /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/citeline
 git pull
 
-podman compose -f database/milvus/docker-compose.yml up -d
-podman ps
-
-cd database
-python milvusdb.py --list-collections
-
+# Load container for Ollama service
+# env > slurm_env.txt
+export OLLAMA_BASE_URL=http://localhost:11434
+export TMPDIR=/n/holylabs/LABS/protopapas_lab/Lab/bbasseri/tmp
+podman load -i /n/holylabs/LABS/protopapas_lab/Lab/bbasseri/ollama_llama3.3.tar
+echo "Images available:"
+podman images
+# podman run -d --log-level=debug --rm --device nvidia.com/gpu=all -p 11434:11434 ollamaserve:latest
+# sleep 30
+# echo "After 30 seconds..."
+# podman container list
+# curl http://localhost:11434/api/generate -d '{"model": "llama3.3:latest", "prompt": "Respond with a single word that is the name of a fruit."}'
+# python dataset_builder.py
+# timestamp=$(date +"%Y%m%d_%H%M%S")
+# echo "ended at: $timestamp"
